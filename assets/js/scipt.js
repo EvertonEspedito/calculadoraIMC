d3135ba64daf23d7216716131f3184f9c73944ca
Everton Espedito <152447787+EvertonEspedito@users.noreply.github.com>
2024-05-30 11:15:11 -0300

    Update scipt.js

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -5,16 +5,19 @@ form.addEventListener('submit', function (event) {
 
     const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
-
+    const socialIcon = document.getElementById('icon-social');
     const bmi = (weight/(height * height)).toFixed(2);
-
+    const screenW = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     const value = document.getElementById('value');
     let descrition = "";
 
     value.classList.add('attetion');
+    
+    socialIcon.classList.add('fa-lg');
 
     document.getElementById('info').classList.remove('hidden');
 
+    /**IMC */
     if (bmi <18.5) {
         descrition = "Cuidao você esta abaixo do peso";
     } else if (bmi >= 18.5 && bmi <25) {
@@ -27,7 +30,12 @@ form.addEventListener('submit', function (event) {
     }else if (bmi >30) {
         descrition = "Você está obeso";
     }
-
+    /**tamanho da tela */
+    if (screenW < 1080) {
+        socialIcon.classList.remove('fa-lg');
+        socialIcon.classList.add('fa-xl');
+    }
+    
     value.textContent = bmi.replace('.',',');
     document.getElementById('descri').textContent = descrition
 });
